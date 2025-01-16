ad10061cce62085b92b08c8a12780394e8a23b9f
5z-lql2 <5zabc4989@gmail.com>
2025-01-16 17:35:33 +0900

    feat: ✨ configを使用するためのロジックを追加

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -11,4 +11,8 @@ export interface Sandbox {
   get_mcSounds: (version: string) => Promise<Sound[]>
   get_mcSoundHash: (hash: string) => Promise<string>
   make_sub_window: () => void
+  loadSettings: () => Promise<any>
+  updateSettings: (partial: Record<string, any>) => void
+  getSetting: <Key extends string>(key: Key) => Promise<any>;
+  setSetting: <Key extends string>(key: Key, value: any) => void;
 }
--- src/preload.ts
+++ src/preload.ts
@@ -7,4 +7,8 @@ contextBridge.exposeInMainWorld('myAPI', {
   get_mcSounds: (version: string) => ipcRenderer.invoke('get_mcSounds', version),
   get_mcSoundHash: (hash: string) => ipcRenderer.invoke('get_mcSoundHash', hash),
   make_sub_window: () => ipcRenderer.invoke('make_sub_window'),
+  loadSettings: () => ipcRenderer.invoke('settings:load'),
+  updateSettings: (partial: any) => ipcRenderer.send('settings:update', partial),
+  getSetting: (key: string): Promise<any> => ipcRenderer.invoke('settings:get', key),
+  setSetting: (key: string, value: any) => ipcRenderer.send('settings:set', { key, value })
 })
