e51b14e2b63dc64751a33f1743a6e0d97a9279bc
NeAc <45559303+nea-c@users.noreply.github.com>
2025-01-22 04:20:42 +0900

    設定画面にバージョン表記追加

--- src/web/Main/Configuration.tsx
+++ src/web/Main/Configuration.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { SettingsIcon, ArrowDownAZIcon, FilterIcon } from '@yamada-ui/lucide'
-import { Drawer, DrawerHeader, DrawerBody, useDisclosure, IconButton, Switch, Text, HStack, Card, CardHeader, CardBody, VStack } from '@yamada-ui/react'
+import { Drawer, DrawerHeader, DrawerBody, useDisclosure, IconButton, Switch, Text, HStack, Card, CardHeader, CardBody, VStack, Spacer, ScrollArea } from '@yamada-ui/react'
 import { ThemeChange } from './ThemeChange'
 import { useTranslation } from 'react-i18next'
 import { LanguageChange } from './LanguageChange'
+import { version } from '../../../package.json'
 
 export const Configuration = () => {
   const { open, onOpen, onClose } = useDisclosure()
@@ -32,49 +33,61 @@ export const Configuration = () => {
     window.myAPI.updateSettings({ holdRatingFilter: v })
   }
 
+  const appVersion = 'v' + version
+
   return (
     <>
       <IconButton onClick={onOpen} icon={<SettingsIcon fontSize="xl" />} />
 
       <Drawer open={open} onClose={onClose} size="lg" placement="right" style={{ userSelect: 'none' }}>
-        <DrawerHeader>{t('settings')}</DrawerHeader>
+        <DrawerHeader>
+          <Text>{t('settings')}</Text>
+        </DrawerHeader>
 
         <DrawerBody>
 
-          <VStack gap={2}>
-            <Card w="full" variant="outline">
-              <LanguageChange />
-            </Card>
+          <ScrollArea w="full" overflowY="scroll">
+            <VStack gap={2}>
+              <Card w="full" variant="outline">
+                <LanguageChange />
+              </Card>
 
-            <Card w="full" variant="outline">
-              <ThemeChange />
-            </Card>
+              <Card w="full" variant="outline">
+                <ThemeChange />
+              </Card>
 
-            <Card w="full" variant="outline">
-              <CardHeader>
-                <HStack>
-                  <ArrowDownAZIcon fontSize="xl" />
-                  <Text>{t('sort')}</Text>
-                </HStack>
-              </CardHeader>
-              <CardBody>
-                <Switch checked={holdSoundsSort} onChange={() => changesetHoldSoundsSort(!holdSoundsSort)}>{t('hold_sounds_sort')}</Switch>
-              </CardBody>
-            </Card>
+              <Card w="full" variant="outline">
+                <CardHeader>
+                  <HStack>
+                    <ArrowDownAZIcon fontSize="xl" />
+                    <Text>{t('sort')}</Text>
+                  </HStack>
+                </CardHeader>
+                <CardBody>
+                  <Switch checked={holdSoundsSort} onChange={() => changesetHoldSoundsSort(!holdSoundsSort)}>{t('hold_sounds_sort')}</Switch>
+                </CardBody>
+              </Card>
 
-            <Card w="full" variant="outline">
-              <CardHeader>
-                <HStack>
-                  <FilterIcon fontSize="xl" />
-                  <Text>{t('rating_filter')}</Text>
-                </HStack>
-              </CardHeader>
-              <CardBody>
-                <Switch checked={holdRatingFilter} onChange={() => changeHoldRatingFilter(!holdRatingFilter)}>{t('hold_rating_filter')}</Switch>
-              </CardBody>
-            </Card>
+              <Card w="full" variant="outline">
+                <CardHeader>
+                  <HStack>
+                    <FilterIcon fontSize="xl" />
+                    <Text>{t('rating_filter')}</Text>
+                  </HStack>
+                </CardHeader>
+                <CardBody>
+                  <Switch checked={holdRatingFilter} onChange={() => changeHoldRatingFilter(!holdRatingFilter)}>{t('hold_rating_filter')}</Switch>
+                </CardBody>
+              </Card>
 
-          </VStack>
+            </VStack>
+          </ScrollArea>
+
+          <Spacer h="full" />
+          <HStack w="full">
+            <Spacer />
+            <Text>{appVersion}</Text>
+          </HStack>
 
         </DrawerBody>
 
